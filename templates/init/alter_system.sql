9a205b1900dc0f3d17d6ded5f2f193e0c80e57fc
Alexis Pentori <alexis@status.im>
2024-10-09 10:46:21 +0200

    metrics: add postgres-exporter and pg-agent
      * postgres-exporter for basic database metrics
      - pg-agent: optional, to identify query performence issues
    
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- templates/init/alter_system.sql
+++ templates/init/alter_system.sql
@@ -2,3 +2,7 @@
 {% for key,val in postgres_ha_alter_system_settings.items() %}
 ALTER SYSTEM SET {{ key }} = '{{ val }}';
 {% endfor %}
+{% if postgres_ha_perf_metrics_exporter_enable %}
+CREATE extension pg_stat_statements;
+ALTER SYSTEM SET shared_preload_libraries = 'pg_stat_statements';
+{% endif %}
